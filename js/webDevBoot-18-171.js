e2e1bfb053b7b044cdd3b36f79c6609d8928206c
sfdeloach <sfdeloach@gmail.com>
2017-02-01 00:22:33 -0500

    todo app almost finished, two bugs remain

--- js/webDevBoot-18-171.js
+++ js/webDevBoot-18-171.js
@@ -2,21 +2,48 @@
 //console.log("linked to webDevBoot-18-171.html");
 //console.log("created by sfdeloach on 01/31/2017");
 
-// Trash slide effects
-$('.trash').hide();
+var itemHead = '<div class="item"><div class="trash"><i class="fa fa-trash" aria-hidden="true"></i></div><div class="details">';
+var itemFoot = '</div></div>';
 
-$('.item').on('mouseenter', function () {
+// Item crossout
+$('.item').on('click', '.details', function () {
+    'use strict';
+    // TODO this is not crossing out text for appended items
+    $(this).toggleClass('crossout');
+});
+
+// Todo slide
+$('#plus').on('click', function () {
+    'use strict';
+    $('input').slideToggle();
+});
+
+// Todo add new item
+$("input").keypress(function (event) {
+    'use strict';
+    if (event.which === 13) {
+        var newItem = $("input").val();
+        $("input").val("");
+        $(".item-group").append(itemHead + newItem + itemFoot);
+    }
+});
+
+// Trash slide
+$('.item-group').on('mouseenter', '.item', function () {
     'use strict';
     $(this).find('.trash').show('slide');
 });
 
-$('.item').on('mouseleave', function () {
+$('.item-group').on('mouseleave', '.item', function () {
     'use strict';
     $(this).find('.trash').hide('slide');
 });
 
-// Todo slide effect
-$('#plus').on('click', function () {
+// Trash delete
+$('.item').on('click', '.trash', function () {
     'use strict';
-    $('input').slideToggle();
+    // TODO this is not removing appended elements
+    $(this).parent('.item').hide('slow', function () {
+        $(this).remove();
+    });
 });
