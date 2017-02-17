bbc1249d4805c7a973c39930216bfc86d7b80201
Steven DeLoach <sfdeloach@gmail.com>
2017-02-17 16:57:46 -0500

    more work with APIs

--- APIs/first_request.js
+++ APIs/first_request.js
@@ -1,8 +1,15 @@
 var request = require('request');
 
-request('http://www.google.com', function (error, response, body) {
+var requestString = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22longwood%2C%20fl%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+
+request(requestString, function (error, response, body) {
   if (!error && response.statusCode == 200) {
-    console.log(body)
+    var parsedBody = JSON.parse(body);
+    var channel = parsedBody["query"]["results"]["channel"];
+    var temp = channel["item"]["condition"]["temp"];
+    var conditions = channel["item"]["condition"]["text"];
+    console.log(channel["title"]);
+    console.log(temp + " degF, " + conditions);
   } else {
     console.log("Aww snap! Something went wrong!");
     console.log(error);
